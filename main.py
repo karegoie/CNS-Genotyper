526a3901e94110f9653bcc1223aed54506cd88f5
Octo Moon <octahedron00@gmail.com>
2024-03-19 14:50:55 +0900

    Cleaning Code 031914

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import click
 from Bio import SeqIO
 import gzip
 import configparser
+import gc
 
 # type hinting for lower version of python
 from typing import List
@@ -13,7 +14,8 @@ from typing import List
 from src.reference import Reference
 from src.aligned import Aligned_Key, Aligned_Read, Aligned
 from src.genotyper_for_reference import Genotyper_For_Reference
-from src.log_writer import write_main_log, write_main_html_log, write_sub_log, write_main_csv_log, get_main_log_name, write_raw_data_log
+from src.log_writer import write_main_log, write_main_html_log, write_sub_log, write_main_csv_log, get_main_log_name, \
+    write_raw_data_log
 import src.globals as glv
 
 DATA_ADDRESS = "./data/"
@@ -21,6 +23,7 @@ REF_ADDRESS = "./ref/"
 GUIDE_RNA_SET_ADDRESS = "./ref/guide_RNA_set.txt"
 REF_SET_ADDRESS = "./ref/reference_seq_set.txt"
 CONFIG_ADDRESS = "./config.txt"
+
 
 # with spans, 3 second for 1000 lines: 20000 for a minute, 1,000,000 ~ 1,500,000 for an hour
 # > total 800 nt of ref, 150 nt for a line: 40,000,000 for a second.
@@ -72,14 +75,17 @@ def get_reference_list_from_file():
 
 def get_file_data_file_list():
     data_file_list = [file_name for file_name in os.listdir(DATA_ADDRESS)
-                    if os.path.isfile(os.path.join(DATA_ADDRESS, file_name))
-                    and file_name[-6:] in ('.fastq', 'stq.gz')]
+                      if os.path.isfile(os.path.join(DATA_ADDRESS, file_name))
+                      and file_name[-6:] in ('.fastq', 'stq.gz')]
 
     for ignore_text in glv.READ_IGNORE:
         if len(ignore_text) > 0:
             data_file_list = [address for address in data_file_list if address.find(ignore_text) < 0]
 
     data_file_list.sort(key=lambda f: int(''.join(filter(str.isdigit, f)) + '0'))
+    print(f"File list: files with '.fastq.gz' or '.fastq' in {DATA_ADDRESS} only\n"
+          f"File list: ignoring keyword {glv.READ_IGNORE} in the name\n"
+          f"File list: {data_file_list}\n")
     return data_file_list
 
 
@@ -112,7 +118,6 @@ def key_for_sorting_err(aligned_read: Aligned_Read):
 
 
 def test_all_input_files():
-
     is_data_exist = False
     is_guide_rna_exist = False
     is_reference_exist = False
@@ -177,12 +182,11 @@ def get_seq_key(seq: str):
 
 
 def get_key_list_of_all_seq(data_file_list: list):
-
     hashmap_seq_key = dict()
     total_reads = 0
 
     for file_no, file_name in enumerate(data_file_list):
-        print(file_no+1, file_name)
+        print(file_no + 1, file_name)
 
         if file_name[-5:] == str("file.fastq.gz")[-5:]:
             read_raw_iter = SeqIO.parse(gzip.open(str(os.path.join(DATA_ADDRESS, file_name)), "rt"), "fastq")
@@ -201,6 +205,9 @@ def get_key_list_of_all_seq(data_file_list: list):
             hashmap_seq_key[seq_key] = None
 
     key_list = list(hashmap_seq_key.keys())
+
+    print(f"Total reads: {total_reads}\n"
+          f"Total unique reads: {len(key_list)}\n")
     return key_list, total_reads
 
 
@@ -211,11 +218,107 @@ def get_aligned_hashmap(seq_key_list, reference_list):
         hashmap[seq_key] = get_best_aligned_key(seq_key, reference_list)
         # print(seq_key, hashmap[seq_key])
         if i % 100 == 0:
-            print("\r", i+1, datetime.datetime.now() - start_time,
-                  (datetime.datetime.now() - start_time) * (len(seq_key_list) - (i+1)) / (i+1), "             ", end="")
+            print("\r", i + 1, datetime.datetime.now() - start_time,
+                  (datetime.datetime.now() - start_time) * (len(seq_key_list) - (i + 1)) / (i + 1), "             ",
+                  end="")
 
     print()
     return hashmap
+
+
+def set_global_variables(read_ignore, err_ratio_max, err_padding_for_seq, cut_pos_from_pam, cut_pos_radius,
+                         phred_meaningful_score_min, pam_distance_max, score_match, score_mismatch, score_gap_open,
+                         score_gap_extend, task_title, open_xlsx_auto, debug):
+    glv.READ_IGNORE = read_ignore
+
+    glv.ERR_RATIO_MAX = err_ratio_max
+    glv.ERR_PADDING_FOR_SEQ = err_padding_for_seq
+    glv.CUT_POS_FROM_PAM = cut_pos_from_pam
+    glv.CUT_POS_RADIUS = cut_pos_radius
+
+    glv.PHRED_MEANINGFUL_MIN = phred_meaningful_score_min
+    glv.PAM_DISTANCE_MAX = pam_distance_max
+
+    glv.MAT = score_match
+    glv.MIS = score_mismatch
+    glv.GAP_OPEN = score_gap_open
+    glv.GAP_EXTEND = score_gap_extend
+
+    glv.TASK_TITLE = task_title
+    glv.OPEN_XLSX_AUTO = open_xlsx_auto
+    glv.DEBUG = debug
+    return
+
+
+def get_genotyper_list_for_reference_list(reference_list: list, file_name: str, aligned_read_list: list):
+    genotyper_list = []
+    for reference in reference_list:
+        genotyper = Genotyper_For_Reference(reference=reference, file_name=file_name)
+        genotyper_list.append(genotyper)
+
+    # count the number of each indel type,
+    # also setting the best aligned_read for each indel type happens here
+    for aligned_read in aligned_read_list:
+        for genotyper in genotyper_list:
+            if genotyper.ref_name == aligned_read.ref_name:
+                genotyper.count(aligned_read)
+    return genotyper_list
+
+
+def get_aligned_read_list_for_file(file_no, file_name, aligned_hashmap, total_file_length,
+                                   total_reads_count, finish_reads_count, start_time_3):
+    if file_name[-5:] == str("file.fastq.gz")[-5:]:
+        read_raw_iter = SeqIO.parse(gzip.open(str(os.path.join(DATA_ADDRESS, file_name)), "rt"), "fastq")
+    elif file_name[-5:] == str(".fastq")[-5:]:
+        read_raw_iter = SeqIO.parse(os.path.join(DATA_ADDRESS, file_name), "fastq")
+    else:
+        return None
+
+    aligned_read_list = []
+    read_raw_list = [read_raw for read_raw in read_raw_iter]
+    for i, read_raw in enumerate(read_raw_list):
+
+        seq = str(read_raw.seq)
+        seq_key = get_seq_key(seq)
+        aligned_key = aligned_hashmap[seq_key]
+        if aligned_key.strand == '-':
+            aligned_read = Aligned_Read(aligned_key, read_raw.reverse_complement(), file_name)
+        else:
+            aligned_read = Aligned_Read(aligned_key, read_raw, file_name)
+
+        # print(aligned_read)
+        aligned_read_list.append(aligned_read)
+
+        # # # # for showing expected time left
+        finish_reads_count += 1
+
+        if (i % 100) == 0:
+            now_time = datetime.datetime.now()
+            delta_time = now_time - start_time_3
+            delta_count = finish_reads_count
+
+            print(f"\r({file_no}/{total_file_length}) "
+                  f"for {file_name}: {((i + 1) / len(read_raw_list)):.3f} / "
+                  f"remaining: {(delta_time / delta_count) * (total_reads_count - finish_reads_count)} "
+                  f"(for this file: {(delta_time / delta_count) * (len(read_raw_list) - (i + 1))}) "
+                  f"(length: {len(read_raw_list)})                              ", end="")
+
+    # # # for showing expected time left / while log writing
+    print(f"\r({file_no}/{total_file_length}) for {file_name}: Complete / "
+          f"Writing log files (length: {len(aligned_read_list)})                              ", end="")
+
+    return aligned_read_list
+
+
+def get_sorted_aligned_read_list(aligned_read_list, genotyper_list):
+    genotyper_map = {}
+    for genotyper in genotyper_list:
+        genotyper_map[genotyper.ref_name] = genotyper
+    aligned_read_list.sort(key=lambda l: l.score, reverse=True)
+    aligned_read_list.sort(key=lambda l: genotyper_map[l.ref_name].count_map[l.indel.indel_type], reverse=True)
+    aligned_read_list.sort(key=lambda l: key_for_sorting_err(l))
+
+    return aligned_read_list
 
 
 @click.command()
@@ -255,161 +358,62 @@ def main(read_ignore, err_ratio_max, err_padding_for_seq, cut_pos_from_pam, cut_
          score_match, score_mismatch, score_gap_open, score_gap_extend,
          task_title, open_xlsx_auto, debug):
     # set global variables
-    glv.READ_IGNORE = read_ignore
-
-    glv.ERR_RATIO_MAX = err_ratio_max
-    glv.ERR_PADDING_FOR_SEQ = err_padding_for_seq
-    glv.CUT_POS_FROM_PAM = cut_pos_from_pam
-    glv.CUT_POS_RADIUS = cut_pos_radius
-
-    glv.PHRED_MEANINGFUL_MIN = phred_meaningful_score_min
-    glv.PAM_DISTANCE_MAX = pam_distance_max
-
-    glv.MAT = score_match
-    glv.MIS = score_mismatch
-    glv.GAP_OPEN = score_gap_open
-    glv.GAP_EXTEND = score_gap_extend
-
-    glv.TASK_TITLE = task_title
-    glv.OPEN_XLSX_AUTO = open_xlsx_auto
-    glv.DEBUG = debug
+    set_global_variables(read_ignore, err_ratio_max, err_padding_for_seq, cut_pos_from_pam, cut_pos_radius,
+                         phred_meaningful_score_min, pam_distance_max,
+                         score_match, score_mismatch, score_gap_open, score_gap_extend,
+                         task_title, open_xlsx_auto, debug)
 
     is_good_to_go = test_all_input_files()
     if not is_good_to_go:
-        input("Press any key to finish...")
+        # input("Press any key to finish...")
         return -1
 
-    # Get sorted file address list from a folder, list[str]
     data_file_list = get_file_data_file_list()
-    print(f"File list: files with '.fastq.gz' or '.fastq' in {DATA_ADDRESS} only\n"
-          f"File list: ignoring keyword {glv.READ_IGNORE} in the name\n"
-          f"File list: {data_file_list}\n")
-
-    # get a list[Reference]
     reference_list = get_reference_list_from_file()
 
-    # # # for counting expected time left,
-    # # # count total number of reads,
-    # # # count total number of finished number of reads,
-    # # # and check the time of initiation
-    '''This function will make a text print: opening large file takes some time'''
-    # # before_start_time = datetime.datetime.now()
-    # total_reads_count, reads_count_list = get_total_number_of_reads(data_file_list=data_file_list)
-    start_time = datetime.datetime.now()
-
+    start_time_0 = start_time_1 = datetime.datetime.now()
     seq_key_list, total_reads_count = get_key_list_of_all_seq(data_file_list)
     finish_reads_count = 0
-    print(f"Total reads: {total_reads_count}\n"
-          f"Total unique reads: {len(seq_key_list)}\n")
+    print(f"\n\tUnique sequence reading: {datetime.datetime.now() - start_time_1}")
+
+    start_time_2 = datetime.datetime.now()
     aligned_hashmap = get_aligned_hashmap(seq_key_list, reference_list)
+    print(f"\n\tUnique sequence aligning: {datetime.datetime.now() - start_time_2}")
 
     # for total result, making list[list[genotyper]]
+    start_time_3 = datetime.datetime.now()
     all_genotyper_list_list = []
-
     debug_data = dict()
 
     for file_no, file_name in enumerate(data_file_list):
         start_time_for_file = datetime.datetime.now()
 
-        # build list[genotyper_For_Ref]
-        genotyper_list = []
-        for reference in reference_list:
-            genotyper = Genotyper_For_Reference(reference=reference, file_name=file_name)
-            genotyper_list.append(genotyper)
+        # build list[Aligned_Read]
+        aligned_read_list = get_aligned_read_list_for_file(file_no+1, file_name, aligned_hashmap, len(data_file_list),
+                                                           total_reads_count, finish_reads_count, start_time_3)
+        # build list[genotyper_For_Ref] with count_result
+        genotyper_list = get_genotyper_list_for_reference_list(reference_list, file_name, aligned_read_list)
 
-        if file_name[-5:] == str("file.fastq.gz")[-5:]:
-            read_raw_iter = SeqIO.parse(gzip.open(str(os.path.join(DATA_ADDRESS, file_name)), "rt"), "fastq")
-        elif file_name[-5:] == str(".fastq")[-5:]:
-            read_raw_iter = SeqIO.parse(os.path.join(DATA_ADDRESS, file_name), "fastq")
-        else:
-            print(f"({file_no + 1}/{len(data_file_list)}) {file_name} is not readable: is it .fastq or .fastq.gz ?")
-            continue
-
-        # # # move the file to the 'used' folder, as quick as possible
-        # os.replace(os.path.join(DATA_ADDRESS, file_name), os.path.join(USED_DATA_ADDRESS, file_name))
-        # not working...
-
-        # build list[Aligned_Read]        
-        aligned_read_list = []
-        read_raw_list = [read_raw for read_raw in read_raw_iter]
-        for i, read_raw in enumerate(read_raw_list):
-
-            seq = str(read_raw.seq)
-            seq_key = get_seq_key(seq)
-            aligned_key = aligned_hashmap[seq_key]
-            if aligned_key.strand == '-':
-                aligned_read = Aligned_Read(aligned_key, read_raw.reverse_complement(), file_name)
-            else:
-                aligned_read = Aligned_Read(aligned_key, read_raw, file_name)
-
-            # print(aligned_read)
-            aligned_read_list.append(aligned_read)
-
-            # # # # for showing expected time left
-            finish_reads_count += 1
-
-            if (i % 100) == 0:
-                now_time = datetime.datetime.now()
-                delta_time = now_time - start_time
-                delta_count = finish_reads_count
-
-                print(f"\r({file_no + 1}/{len(data_file_list)}) "
-                      f"for {file_name}: {((i+1)/len(read_raw_list)):.3f} / "
-                      f"remaining: {(delta_time/delta_count)*(total_reads_count-finish_reads_count)} "
-                      f"(for this file: {(delta_time/delta_count)*(len(read_raw_list)-(i+1))}) "
-                      f"(length: {len(read_raw_list)})                              ", end="")
-
-        # # # for showing expected time left / while log writing
-        print(f"\r({file_no + 1}/{len(data_file_list)}) for {file_name}: Complete / "
-              f"Writing log files (length: {len(aligned_read_list)})                              ", end="")
-
-        # count the number of each indel type,
-        # also setting the best aligned_read for each indel type happens here
-        for aligned_read in aligned_read_list:
-            for genotyper in genotyper_list:
-                if genotyper.ref_name == aligned_read.ref_name:
-                    genotyper.count(aligned_read)
+        # log order: err for the last / biggest indel type first / higher score / higher phred score / longer one first
+        sorted_aligned_read_list = get_sorted_aligned_read_list(aligned_read_list, genotyper_list)
 
         # add the file result to the total result
         all_genotyper_list_list.append(genotyper_list)
 
-        # # Sorting Line Set List!
-        # # err for the last / biggest indel type first / higher score / higher phred score / longer one first
-        # # just for the log file
-        genotyper_map = {}
-        for genotyper in genotyper_list:
-            genotyper_map[genotyper.ref_name] = genotyper
-        aligned_read_list.sort(key=lambda l: l.score, reverse=True)
-        aligned_read_list.sort(key=lambda l: genotyper_map[l.ref_name].count_map[l.indel.indel_type], reverse=True)
-        aligned_read_list.sort(key=lambda l: key_for_sorting_err(l))
-
         # Writing sub log
         for genotyper in genotyper_list:
-            write_sub_log(aligned_read_list=[l for l in aligned_read_list if l.ref_name == genotyper.ref_name],
+            write_sub_log(aligned_read_list=[l for l in sorted_aligned_read_list if l.ref_name == genotyper.ref_name],
                           genotyper=genotyper, file_name=file_name)
-
-        # # for debug data
-        if glv.DEBUG:
-            pos_phred_score = [0] * 25
-            pos_error_count = [0] * 25
-
-            for aligned_read in aligned_read_list:
-                if len(aligned_read) < 20:
-                    print(aligned_read)
-                    continue
-                for i in range(-10, 10):
-                    pos_phred_score[i] += (ord(aligned_read.phred_line[i]) - glv.PHRED_ENCODING)
-
-                    if aligned_read.match_line[i] != '|':
-                        pos_error_count[i] += 1
-
         # # for showing time used
+        finish_reads_count += len(sorted_aligned_read_list)
+
         end_time_for_file = datetime.datetime.now()
         print(f"\r({file_no + 1}/{len(data_file_list)}) for {file_name}: Complete / Log written / "
-              f"{end_time_for_file - start_time} ({end_time_for_file - start_time_for_file} for this file) is passed "
-              f"(length: {len(aligned_read_list)})       ")
+              f"{end_time_for_file - start_time_0} ({end_time_for_file - start_time_for_file} for this file) is passed "
+              f"(length: {len(sorted_aligned_read_list)})       ")
 
         # end.
+    print(f"\n\tAll read matching: {datetime.datetime.now() - start_time_3}")
 
     # Writing total log
     write_main_log(genotyper_list_list=all_genotyper_list_list, total_length=total_reads_count)
@@ -418,25 +422,17 @@ def main(read_ignore, err_ratio_max, err_padding_for_seq, cut_pos_from_pam, cut_
     write_raw_data_log(genotyper_list_list=all_genotyper_list_list, debug_data=debug_data)
 
     # # for showing time used
-    print(f"Work Completed! (total time: {datetime.datetime.now() - start_time})")
+    print(f"Work Completed!\n\t(total time: {datetime.datetime.now() - start_time_0})")
 
-    # # delete the file of 'program is running here'
-    # os.remove(os.path.join(DATA_ADDRESS, ".program_is_running_here"))
-
-    #
     if glv.OPEN_XLSX_AUTO:
         os.system(f"start EXCEL.EXE {get_main_log_name('xlsx')}")
     # input("Press enter to finish the program...")
 
 
 if __name__ == '__main__':
-    print("CNS-Genotyper ver. "+glv.VERSION)
+    print("CNS-Genotyper ver. " + glv.VERSION)
     print()
     main()
 
     # this is how 'click' works...
     print("Things never works below here... << error message")
-
-
-
-
